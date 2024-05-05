f9172a44f1ee13579a035c28499cdc9cb4f400e5
laurendecamps <131840329+laurendecamps@users.noreply.github.com>
2024-05-04 21:27:59 -0600

    Update mission.js

--- mission.js
+++ mission.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
   const themeSelector = document.getElementById('themeSelector');
-  const logo = document.getElementById('logo');
+  const logo = document.querySelector('.logo');
 
   function changeTheme() {
     const selectedTheme = themeSelector.value;
 
     if (selectedTheme === 'dark') {
       document.body.classList.add('dark');
-      logo.src = 'white-logo.png';
+      logo.src = 'byui-logo_white.webp'; // Assuming the new logo image name is 'byui-logo_white.webp'
     } else {
       document.body.classList.remove('dark');
-      logo.src = 'dark-logo.png';
+      logo.src = 'byui-logo_blue.webp'; // Original logo image name
     }
   }
 
